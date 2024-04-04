27bff11736091ddc4cf589ffe92e21ada4b31ffa
MAHESH KADALI <101107025+MAHESH-KADALI@users.noreply.github.com>
2024-04-04 12:59:21 +0530

    Add files via upload
    Signed-off-by: MAHESH KADALI <101107025+MAHESH-KADALI@users.noreply.github.com>

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -1,4 +1,3 @@
-# (c) adarsh-goel
 import os
 from os import getenv, environ
 from dotenv import load_dotenv
@@ -9,32 +8,35 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
-    name = str(getenv('name', 'filetolinkbot'))
+    API_ID = int(getenv('API_ID', '4857766'))
+    API_HASH = str(getenv('API_HASH', '6c3c6facf5598a4b318e138f8c407028'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6459372751:AAEqxTT4ewo5nuRjH5u4t_K8umfMFkpk0g0'))
+    name = str(getenv('name', 'maheshfiletolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
-    PORT = int(getenv('PORT', 8080))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001963446260'))
+    PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1596559467").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'maheshsirop'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Filesstreambot:eHtqPEVohrO3YAS3@cluster0.hdxt929.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'hexafreinds'))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))      
+    SHORTLINK_URL = getenv('SHORTLINK_URL', 'publicearn.com')
+    SHORTLINK_API = getenv('SHORTLINK_API', 'ccdd7d73458653f07aecbaa1d8159457965391b4')
+    TUTORIAL_URL = getenv('TUTORIAL_URL', 'https://t.me/hexafreinds/60883')
